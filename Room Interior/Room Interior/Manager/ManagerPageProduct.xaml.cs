80cef1b94961654d33a8deecee549a7061bce966
sindroid15 <sin.droid15@gmail.com>
2021-10-26 11:36:24 +0300

    RoomINterior restored ManagerPageUser 26.10.2021 11:36

---
+++ Room Interior/Room Interior/Manager/ManagerPageProduct.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Room_Interior.Manager
+{
+    /// <summary>
+    /// Логика взаимодействия для ManagerPageProduct.xaml
+    /// </summary>
+    public partial class ManagerPageProduct : Page
+    {
+        public ManagerPageProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackFrm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbFiltrationCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbSortingPrice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnLastPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEditProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LvProduct_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
